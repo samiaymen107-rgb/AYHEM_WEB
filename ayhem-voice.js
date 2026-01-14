1d7e64f8250d79ab3173e612c90b22aaf0101a26
samiaymen107-rgb <samiaymen107@gmail.com>
2026-01-14 11:24:41 +0100

    Add speak function for text-to-speech in Arabic

---
+++ ayhem-voice.js
@@ -0,0 +1,8 @@
+function speak(text){
+  if(!("speechSynthesis" in window)) return;
+  const u=new SpeechSynthesisUtterance(text);
+  u.lang="ar-SA";
+  u.rate=1;
+  speechSynthesis.cancel();
+  speechSynthesis.speak(u);
+}
