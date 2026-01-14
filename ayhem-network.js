2ed9aad9135cf0a81ac4df6f5a8b09bc62c14dbf
samiaymen107-rgb <samiaymen107@gmail.com>
2026-01-14 15:11:35 +0100

    Update ayhem-network.js

--- ayhem-network.js
+++ ayhem-network.js
@@ -1,16 +1,22 @@
 import {getMemory, saveMemory} from "./ayhem-memory.js";
+import {extractFacts, smartCompress, getSmartContext} from "./ayhem-smart-memory.js";
 
 const API = "https://autumn-brook-5828.samiaymen720.workers.dev";
 
 export async function askAYHEM(prompt, persona){
+  extractFacts(prompt);
+
   const memory = getMemory();
+  const compressedMemory = smartCompress(memory);
+  const smart = getSmartContext();
 
   const r = await fetch(API,{
     method:"POST",
     headers:{"Content-Type":"application/json"},
     body: JSON.stringify({
       prompt,
-      memory,
+      memory: compressedMemory,
+      smart,
       persona,
       identity:"AYHEM"
     })
