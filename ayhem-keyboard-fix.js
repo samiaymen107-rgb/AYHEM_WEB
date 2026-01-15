5eb145f35c2788333d0993221b8eec9eec8cfda0
samiaymen107-rgb <samiaymen107@gmail.com>
2026-01-15 17:15:46 +0100

    Update ayhem-keyboard-fix.js

--- ayhem-keyboard-fix.js
+++ ayhem-keyboard-fix.js
@@ -1,1 +1,6 @@
-
+document.addEventListener("focusin", () => {
+  document.body.classList.add("kb");
+});
+document.addEventListener("focusout", () => {
+  document.body.classList.remove("kb");
+});
