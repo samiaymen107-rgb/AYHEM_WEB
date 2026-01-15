ebb233a6ca121a5033098cc43dd922218a784d85
samiaymen107-rgb <samiaymen107@gmail.com>
2026-01-15 13:35:24 +0100

    Update ayhem-safe-mobile.js

--- ayhem-safe-mobile.js
+++ ayhem-safe-mobile.js
@@ -1,1 +1,9 @@
-
+(function(){
+  const bar=document.querySelector(".input-bar");
+  if(!bar) return;
+  bar.style.position="fixed";
+  bar.style.bottom="0";
+  bar.style.left="0";
+  bar.style.right="0";
+  bar.style.zIndex="999";
+})();
