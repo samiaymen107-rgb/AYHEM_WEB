9441c06b3782306f2826f9b8319eb362ae785ea2
samiaymen107-rgb <samiaymen107@gmail.com>
2026-02-26 03:27:42 +0100

    Create AYHEM_INTELLIGENCE_ARCH.js

---
+++ AYHEM_WEB/AYHEM_INTELLIGENCE_SYSTEM/AYHEM_INTELLIGENCE_SYSTEM/AYHEM_INTELLIGENCE_SYSTEM/AYHEM_INTELLIGENCE_ARCH.js
@@ -0,0 +1,24 @@
+/*
+AYHEM Intelligence Architecture
+Integrated Layer
+*/
+
+const INTELLIGENCE_ARCH={
+
+ENC:false,
+
+CORE:true,
+
+AI:true,
+
+NODES:true,
+
+UI:false,
+
+engine:"ARGUS",
+
+mode:"AUTONOMOUS"
+
+}
+
+export default INTELLIGENCE_ARCH
