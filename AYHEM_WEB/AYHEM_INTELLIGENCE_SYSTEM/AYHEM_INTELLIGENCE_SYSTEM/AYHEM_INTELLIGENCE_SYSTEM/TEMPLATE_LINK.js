cd595df35dde96ec358d1af10533b9b176adc96b
samiaymen107-rgb <samiaymen107@gmail.com>
2026-02-26 03:35:28 +0100

    Create TEMPLATE_LINK.js

---
+++ AYHEM_WEB/AYHEM_INTELLIGENCE_SYSTEM/AYHEM_INTELLIGENCE_SYSTEM/AYHEM_INTELLIGENCE_SYSTEM/TEMPLATE_LINK.js
@@ -0,0 +1,15 @@
+/*
+AYHEM Link Template
+قالب لربط النظم الداخلية مع المستقبل AI/Recon
+*/
+
+const TEMPLATE_INTEL = {
+    system: "AYHEM",
+    layer: "CORE",
+    engine: "ARGUS",
+    mode: "TEMPLATE",
+    status: "STORED",
+    last_sync: null
+}
+
+export default TEMPLATE_INTEL;
